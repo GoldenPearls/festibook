f190f90adc353181add1cc832047fb4b6b8f974c
[GoldenPearls] <prettylee620@naver.com>
2023-09-23 18:03:02 +0900

    [backend] 토큰 발급을 위한 로그인 보안적인 부분 작업
     WebSecurityConfigurerAdapter에서 문제가 생기는 것 같아 하기 수정 중 에러에러 2

--- src/main/java/com/in4mation/festibook/config/SecurityConfig.java
+++ src/main/java/com/in4mation/festibook/config/SecurityConfig.java
@@ -3,37 +3,23 @@ package com.in4mation.festibook.config;
 import com.in4mation.festibook.jwt.JwtUtils;
 import com.in4mation.festibook.service.login.LoginServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration // 해당 클래스를 Spring Configuration으로 등록
 @EnableWebSecurity // Spring Security를 활성화
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 메소드 수준에서의 보안을 활성화
-public class SecurityConfiguration extends SecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
    /* @Autowired // JwtUtils Bean을 주입
     private JwtUtils jwtUtils;
@@ -41,22 +27,15 @@ public class SecurityConfiguration extends SecurityConfigurerAdapter {
     private UserDetailsService loginService;*/
 
     private final JwtUtils jwtUtils;
-    private final UserDetailsService userDetailsService;
-
-   /* @Autowired
-    public SecurityConfiguration(JwtUtils jwtUtils, LoginServiceImpl loginService) {
-        this.jwtUtils = jwtUtils;
-        this.loginService = loginService;
-    }*/
+    private final LoginServiceImpl loginService;
 
     @Autowired
-    public SecurityConfiguration(JwtUtils jwtUtils, @Qualifier("loginServiceImpl") UserDetailsService userDetailsService) {
+    public SecurityConfig(@Lazy JwtUtils jwtUtils, LoginServiceImpl loginService) {
         this.jwtUtils = jwtUtils;
-        this.userDetailsService = userDetailsService;
+        this.loginService = loginService;
     }
 
-
-/*    @Override // HttpSecurity를 사용하여 Web Security 설정을 오버라이드한다.
+    @Override // HttpSecurity를 사용하여 Web Security 설정을 오버라이드한다.
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() // CSRF(사이트 간 요청 위조) 공격 방어를 비활성화
@@ -66,46 +45,8 @@ public class SecurityConfiguration extends SecurityConfigurerAdapter {
                 .authorizeRequests() // HttpServletRequest에 따라 접근을 제한
                 .antMatchers("/**").permitAll() // 모든 경로에 대해 접근을 허용
                 .anyRequest().authenticated(); // 그 외의 모든 요청은 인증을 요구
-    }*/
-
-    //HttpSecurity 구성
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
     }
 
-    // 웹 보안 구성
-    // WebSecurityCustomizer에서 WebSecurityCustomizer사용자 정의하는 데 사용할 수 있는 콜백 인터페이스
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
-    }
-
-   /* @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-                .build();
-    }*/
-
-    /*@Bean
-    public UserDetailsManager users(DataSource dataSource) {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .build();
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-        users.createUser(user);
-        return users;
-    }
-*/
-/*
     // void로 쓰면 안됨!!!!
     @Autowired // AuthenticationManagerBuilder를 주입받아 사용자 세부 서비스를 설정
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -117,11 +58,10 @@ public class SecurityConfiguration extends SecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean(); // 기본 AuthenticationManager Bean을 반환
-    }*/
+    }
 
     @Bean // PasswordEncoder Bean을 생성하여 Spring Context에 등록
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 비밀번호를 인코딩하는데 사용되는 BCryptPasswordEncoder를 반환
     }
 }
-
